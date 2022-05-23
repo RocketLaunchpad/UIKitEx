bdceb6e6dfe286f8f9b7548ec964be3c10ab78de
Ilya Belenkiy <ilya@harmonicsense.com>
2022-05-23 10:47:37 -0400

    Require at least iOS 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "UIKitEx",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
     ],
     products: [
         .library(
